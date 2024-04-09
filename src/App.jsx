345829eeb4e0ca2a402e0a884f84f21d4d097e69
striker1826 <alstjq123579@daum.net>
2024-04-09 22:00:29 +0900

    🚨 esLint 설정 변경
    - object-curly-newline: off
    - react/prop-types: off

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,8 @@
-import React, { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '../public/vite.svg';
-import './App.css';
+import React from 'react';
+import CreatedRolloingPage from './pages/createdRollingPage';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((c) => c + 1)}>
-          count is
-          {count}
-        </button>
-        <p>
-          Edit
-          <code>src/App.jsx</code>
-          and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <CreatedRolloingPage />;
 }
 
 export default App;
---
+++ src/components/createdRollingPage/EmojiBadge.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { styled } from 'styled-components';
+
+function EmojiBadge() {
+  return (
+    <Container>
+      <StyledText>👍 24</StyledText>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  padding: 8px 15px;
+  border-radius: 32px;
+  background: rgba(0, 0, 0, 0.54);
+`;
+
+const StyledText = styled.span`
+  font-size: 16px;
+  font-weight: 400;
+  color: white;
+`;
+
+export default EmojiBadge;
