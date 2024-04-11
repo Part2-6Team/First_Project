3723334f91f2481b0a364ed0208fbb96eef75c58
striker1826 <alstjq123579@daum.net>
2024-04-11 17:22:03 +0900

    💄 사용하지 않는 Container 컴포넌트 제거

--- src/components/createdRollingPage/Container.jsx
+++
@@ -1,9 +0,0 @@
-import { styled } from 'styled-components';
-
-export const Container = styled.main`
-  background-image: ${({ bgImg }) => (bgImg ? `url(${bgImg})` : 'none')};
-  //   background-color: var(--Orange-200);
-  padding: 80px 0;
-  min-height: 100%;
-  height: auto;
-`;
