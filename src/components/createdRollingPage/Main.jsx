799563559c5f23a3bb28021609de73a66f64f935
striker1826 <alstjq123579@daum.net>
2024-04-11 15:56:28 +0900

    💄 생성된 롤링페이퍼 리스트 Main 및 카드 컴포넌트 UI 변경
    - Main 컴포넌트에 background-image 속성 추가
    - Main 컴포넌트를 display: grid에서 display:flex로 변경

--- src/components/createdRollingPage/Main.jsx
+++ src/components/createdRollingPage/Main.jsx
@@ -10,24 +10,31 @@ function Main() {
         <AddCard />
         <Card />
         <Card />
+        <Card />
+        <Card />
+        <Card />
       </CardContainer>
     </Container>
   );
 }
 
 const Container = styled.main`
+  background-image: ${({ bgImg }) => (bgImg ? `url(${bgImg})` : 'none')};
   background-color: var(--Orange-200);
-  display: flex;
-  justify-content: center;
-  padding: 80px 0 246px;
-  height: 100%;
+  padding: 80px 0;
+  min-height: 100%;
+  height: auto;
 `;
 
 const CardContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  margin: 0 auto;
+  //   display: grid;
+  //   grid-template-columns: repeat(3, 1fr);
+  //   gap: 16px;
+  display: flex;
+  flex-wrap: wrap;
   gap: 16px;
+  justify-content: center;
+  height: auto; /* 부모 요소의 높이를 자식 요소에 맞게 설정합니다. */
 `;
 
 export default Main;
