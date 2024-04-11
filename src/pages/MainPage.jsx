579d3f72856939ad1c22d1828038ea5896bae407
은지 <103625348+eunji-0623@users.noreply.github.com>
2024-04-11 17:51:23 +0900

    ✨ 메인 페이지 레이아웃 구현
    - 글로벌 네비게이션 배치
    - 사이트 소개 부분 배치
    - 구경해보기 버튼은 추후 추가 예정

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,34 @@
+import { styled } from 'styled-components';
+import GlobalNav from '../components/GlobalNav';
+import SiteInfo from '../components/SiteInfo';
+
+function MainPage() {
+  return (
+    <div>
+      <GlobalNav hasButton={true} />
+      <Content>
+        <SiteInfoWrapper>
+          <SiteInfo order={1} />
+          <SiteInfo order={2} />
+        </SiteInfoWrapper>
+        {/* "구경해보기 버튼" */}
+      </Content>
+    </div>
+  );
+}
+
+const Content = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 50px;
+`;
+
+const SiteInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 1200px;
+  gap: 30px;
+`;
+
+export default MainPage;
