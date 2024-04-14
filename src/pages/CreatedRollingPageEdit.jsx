943d6a5c9dc8dbf6cd73acb61463e1b99c8034e0
striker1826 <alstjq123579@daum.net>
2024-04-14 16:30:49 +0900

    💄 URL 공유 기능 수정 - createdRollingPageEdit 페이지에 적용
    - URL 공유 버튼 클릭 시 UrlPharases 컴포넌트를 나타나게 한 후 4초 뒤에 사라지게 만들었습니다.
    
    - URL 공유 버튼 클릭 시 현재 URL이 클립보드에 저장되도록 만들었습니다.

--- src/pages/CreatedRollingPageEdit.jsx
+++ src/pages/CreatedRollingPageEdit.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { styled } from 'styled-components';
 
 import Header from '../components/createdRollingPage/Header';
 import Main from '../components/createdRollingPageEdit/Main';
 import GlobalNav from '../components/GlobalNav';
+import UrlCopyPhrases from '../components/createdRollingPage/UrlCopyPharases';
 
 function CreatedRollingPageEdit() {
+  const [isUrlSharedPharases, setIsUrlSharedPharases] = useState(false);
+
+  const handleCopyClipBoard = async (text) => {
+    await navigator.clipboard.writeText(text);
+  };
+
+  const onClickUrlShared = () => {
+    setIsUrlSharedPharases(true);
+    handleCopyClipBoard(window.location.href);
+
+    setTimeout(() => {
+      setIsUrlSharedPharases(false);
+    }, 4000);
+  };
+
   return (
     <Container>
       <GlobalNav />
-      <Header />
+      <Header
+        handleOpenUrlShared={onClickUrlShared}
+        isUrlSharedPharases={isUrlSharedPharases}
+      />
       <Main />
+      {isUrlSharedPharases && <UrlCopyPhrases />}
     </Container>
   );
 }
