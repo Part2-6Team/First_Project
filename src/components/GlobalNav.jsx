1c12f1b4c134788bd25aa543ced8bb15069f10f7
은지 <103625348+eunji-0623@users.noreply.github.com>
2024-04-16 10:49:19 +0900

    🩹 GlobalNav 버튼이 없는 경우 디자인 수정
    - GlobalNav 버튼이 없는 경우 로고가 가운데 나오던 현상 수정
    - ProfileSelect.jsx에 import 부분 오타 수정

--- src/components/GlobalNav.jsx
+++ src/components/GlobalNav.jsx
@@ -6,7 +6,7 @@ import device from '../config';
 
 function GlobalNav({ hasButton = false }) {
   return (
-    <Nav>
+    <Nav $hasButton={hasButton}>
       <Logo>
         <img className="logoImage" src={logoImage} alt="로고이미지" />
         <div className="logoTitle">Rolling</div>
@@ -20,11 +20,13 @@ function GlobalNav({ hasButton = false }) {
 
 const Nav = styled.nav`
   display: flex;
-  justify-content: center;
-  gap: 940px;
+  justify-content: ${({ $hasButton }) =>
+    $hasButton ? 'center' : 'flex-start'};
+  gap: ${({ $hasButton }) => ($hasButton ? '940px' : '0')};
   align-items: center;
   height: 64px;
   border-bottom: 2px solid var(--Gray-100);
+  padding: ${({ $hasButton }) => ($hasButton ? '0' : '0 360px')};
 
   @media ${device.pc_small} {
     justify-content: space-between;
