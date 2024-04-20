dac5defb421b9cacd6aee336ff5ccb7d2b6b7b70
striker1826 <alstjq123579@daum.net>
2024-04-20 12:12:19 +0900

    createdRollingPageEdit 무한스크롤 구현

--- src/components/createdRollingPageEdit/Main.jsx
+++ src/components/createdRollingPageEdit/Main.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect, useCallback } from 'react';
 import { styled } from 'styled-components';
 import { useParams, useNavigate } from 'react-router-dom';
