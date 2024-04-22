1f603ac9500b36ef85fd79eea52d9bd619e3d057
ehdus3255 <rlaehdus3255@naver.com>
2024-04-23 03:26:39 +0900

    api 데이터 전송 목록 수정

--- src/pages/MakePostPage.jsx
+++ src/pages/MakePostPage.jsx
@@ -57,7 +57,7 @@ function PostPage() {
     const data = {
       team: '5-6',
       name,
-      backgroundColor: toggleValue === 'color' ? selectedColor : 'beige',
+      backgroundColor: toggleValue === 'color' ? selectedColor : null,
       backgroundImageURL: toggleValue === 'img' ? selectedImg : null,
     };
 
