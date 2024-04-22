057a8b8d1a8daa3c852efdb5c096f82cbbe5fb61
imsoohyeok <skilldoy2@naver.com>
2024-04-22 15:11:55 +0900

    --

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,7 @@ import SendMessagePage from './pages/SendMessagePage';
 import CreatedRollingPage from './pages/CreatedRollingPage';
 import CreatedRolloingPageEdit from './pages/CreatedRollingPageEdit';
 import ListPage from './pages/ListPage';
-import MakePostPage from './pages/MakePostPage';
 import PostPage from './pages/MakePostPage';
-
 
 function App() {
   return (
@@ -19,8 +17,7 @@ function App() {
         <Route path="post/:id/edit" element={<CreatedRolloingPageEdit />} />
         <Route path="post/:id/message" element={<SendMessagePage />} />
         <Route path="list" element={<ListPage />} />
-        <Route path="post" elemet={<MakePostPage />} 
-        <Route path="post" elemet={<PostPage />} />
+        <Route path="post" element={<PostPage />} />
       </Routes>
     </BrowserRouter>
   );
